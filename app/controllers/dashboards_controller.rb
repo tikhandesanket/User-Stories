ecfb11a2f5f8642ad93b33e103d5cbd06877d728
tikhandesanket <sanket.tikhande@gmail.com>
2020-02-01 14:40:38 +0530

    added the faker gem and data

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -2,16 +2,16 @@ class DashboardsController < ApplicationController
 	before_action :authenticate_user!
 
 
-	def index
-	
-		if params[:search].present?
-		
-		
-		@users = User.where("age = ? OR gender = ? OR country = ?", params[:age],params[:gender],params[:country])
-
+	def index	
+		if params[:search].present?		
+			@users = User.where("age = ? OR gender = ? OR country = ?", params[:age],params[:gender],params[:country])
 		else
-				@users = User.all
+			@users = User.all
+		end
 
+		respond_to do |format|
+		format.html
+		format.js			
 		end
 	end
 
